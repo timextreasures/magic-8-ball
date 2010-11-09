79a7039eee0b9c0b630d706e8b8200106bf3abce
pyJIoH <pyJIoH@gmail.com>
2010-11-09 08:22:18 +0000

    localization fix

--- gen/pyjioh/apps/R.java
+++ gen/pyjioh/apps/R.java
@@ -23,19 +23,18 @@ public final class R {
         public static final int triangle=0x7f020003;
     }
     public static final class id {
-        public static final int FrameLayout=0x7f080000;
-        public static final int MessageImage=0x7f080001;
-        public static final int shake=0x7f080002;
+        public static final int MessageTextView=0x7f080000;
+        public static final int shake=0x7f080001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int shake=0x7f070000;
+        public static final int menu=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int settings_menu_title=0x7f050001;
+        public static final int shake_me_title=0x7f050001;
         public static final int shake_menu_title=0x7f050002;
     }
 }
