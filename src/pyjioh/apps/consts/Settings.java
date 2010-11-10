67db91685f766cc91e808dd7300c831cf7006751
pyJIoH <pyJIoH@gmail.com>
2010-11-10 11:36:03 +0000

    add settings and sensor using

---
+++ src/pyjioh/apps/consts/Settings.java
@@ -0,0 +1,7 @@
+package pyjioh.apps.consts;
+
+public final class Settings {
+	public static final int FADE_DURATION = 1500;
+	public static final int START_OFFSET = 1000;
+	public static final int VIBRATE_TIME = 200;
+}
