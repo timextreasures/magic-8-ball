2c1f7c92b1b938352f173a645df0dc23222dfc1e
pyJIoH@gmail.com <pyJIoH@gmail.com@097f25a3-e915-7721-a325-05ab1e4e7884>
2010-11-12 13:26:24 +0000

    sensor in the progress

--- trunk/src/pyjioh/apps/consts/Settings.java
+++ trunk/src/pyjioh/apps/consts/Settings.java
@@ -4,5 +4,6 @@ public final class Settings {
 	public static final int FADE_DURATION = 1500;
 	public static final int START_OFFSET = 1000;
 	public static final int VIBRATE_TIME = 200;
-	public static final int SHAKE_COUNT = 3;
+	public static final float SHAKE_FORCE = 0.2f;
+	public static final int SHAKE_TIME = 1500;
 }
