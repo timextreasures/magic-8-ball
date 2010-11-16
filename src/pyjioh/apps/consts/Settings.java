9b5fcd0a0b19984d7ae9899014008b9e27956d5b
pyJIoH <pyJIoH@gmail.com>
2010-11-16 19:29:30 +0000

    shake it now! version 1.0

--- src/pyjioh/apps/consts/Settings.java
+++ src/pyjioh/apps/consts/Settings.java
@@ -3,7 +3,7 @@ package pyjioh.apps.consts;
 public final class Settings {
 	public static final int FADE_DURATION = 1500;
 	public static final int START_OFFSET = 1000;
-	public static final int VIBRATE_TIME = 200;
-	public static final float SHAKE_FORCE = 0.2f;
-	public static final int SHAKE_COUNT = 3;
+	public static final int VIBRATE_TIME = 250;
+	public static final double THRESHOLD = 2.75d;
+	public static final int SHAKE_COUNT = 4;
 }
